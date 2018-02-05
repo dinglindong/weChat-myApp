1217aa8f95c681d028930df3f173853a676b9744
dinglindong <258231674@qq.com>
2018-02-05 13:18:41 +0800

    add map,photo

--- pages/demo_request/request.js
+++ pages/demo_request/request.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    src:''
   },
 
   /**
@@ -27,6 +27,46 @@ Page({
       },
       success: function (res) {
         console.log('请求成功了')
+      }
+    })
+  },
+  mapShow(){
+    wx.getLocation({
+      type: 'gcj02',
+      altitude:true,
+      success: function (res) {
+        console.log(res.accuracy);
+        var latitude = res.latitude
+        var longitude = res.longitude
+        var speed = res.speed
+        var accuracy = res.accuracy
+        wx.openLocation({
+          latitude: latitude,
+          longitude: longitude,
+          scale: 28
+        })
+      }
+    });
+    // wx.chooseLocation({
+    //   success:function(res){
+    //     console.log(res)
+    //   }
+    // })
+ 
+  },
+  openPhoto(){
+    let t = this;
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths
+        console.log(res,11111111111111);
+        t.setData({
+          src: tempFilePaths
+        });
       }
     })
   },
