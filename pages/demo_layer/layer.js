fd09a70977530b9f49d08af25618325698a98741
dinglindong <258231674@qq.com>
2018-02-05 15:07:58 +0800

    add 享获取当前

--- pages/demo_layer/layer.js
+++ pages/demo_layer/layer.js
@@ -1,6 +1,6 @@
 //index.js
 //获取应用实例
-const app = getApp()
+const app = getApp();
 
 Page({
 
@@ -12,6 +12,17 @@ Page({
     this.dialog = this.selectComponent("#dialog");
   },
 
+  onLoad: function (options) {
+    var pages = getCurrentPages()    //获取加载的页面
+
+    var currentPage = pages[pages.length-1]    //获取当前页面的对象
+
+    var url = currentPage.route    //当前页面url
+
+    var options = currentPage.options    //如果要获取url中所带的参数可以查看options
+    
+    console.log(currentPage);
+  },
   showDialog() {
     this.dialog.showDialog();
   },
@@ -25,6 +36,13 @@ Page({
   _confirmEvent() {
     console.log('你点击了确定');
     this.dialog.hideDialog();
+  },
+  onShareAppMessage: function () {
+    return {
+      "title": "分享转发666",
+      "path": "pages/discover/discover"
+    }
+    // console.log('只有定义了该函数右上角才有"转发"按钮样式');
+    // 方法内返回一个Object{title:'',path:''}
   }
-
 })
