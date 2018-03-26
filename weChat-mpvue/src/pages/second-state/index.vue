b6f1808a6720bcab7bc0426e806a6aca4c1e5053
ding lindong <258231674@qq.com>
2018-03-26 23:54:43 +0800

    change

--- weChat-mpvue/src/pages/second-state/index.vue
+++ weChat-mpvue/src/pages/second-state/index.vue
@@ -1,7 +1,9 @@
 <template>
   <section class="conainter">
     <h3>发布成功！</h3>
-    您的抽奖码是：123456
+    <p>您的抽奖码是：123456</p>
+    <p>您可以在“唯医社区”服务号内回复进行抽奖。</p>
+    <button>预览分享图片</button>
   </section>
 </template>
 
@@ -12,5 +14,10 @@
 </script>
 
 <style>
+  .conainter{
+    width:100%;
+    padding:100rpx 40rpx;
+    border:1px solid #f00;
 
+  }
 </style>
