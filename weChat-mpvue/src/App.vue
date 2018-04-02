bd8fb49a3313a00728f19aaf2eb9a90a07f51f38
dinglindong <258231674@qq.com>
2018-04-02 15:04:35 +0800

    add get openid

--- weChat-mpvue/src/App.vue
+++ weChat-mpvue/src/App.vue
@@ -7,6 +7,27 @@ export default {
     wx.getStorageSync('logs', logs)
 
     console.log('app created and cache logs by setStorageSync')
+    /*前端获取openId的方法*/
+    //*调取登录信息获取code*//
+    wx.login({
+      success: function(loginRes) {
+        if (loginRes.code) {
+          //调用系统验证，在向微信获取openid
+          wx.getUserInfo({
+            success:function(res){
+              var APPID = 'wxaeb208664e642355';
+              var SECRET = '1275d4508ae8d611c9ba026ac01ff26f';
+              wx.request({
+                url: 'https://api.weixin.qq.com/sns/jscode2session?appid='+APPID+'&secret='+SECRET+'&js_code='+loginRes.code+'&grant_type=authorization_code',
+                success:function(response){
+                  console.log('openid:' + response.data.openid);
+                },
+              })
+            }
+          })
+        }
+      }
+    });
   }
 }
 </script>
