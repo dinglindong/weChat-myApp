51b1cc60cb1c123f84e0d4d1c9a58411400ec15a
dinglindong <258231674@qq.com>
2018-04-03 13:08:28 +0800

    change tagalog

--- weChat-demo/pages/demo_request/request.js
+++ weChat-demo/pages/demo_request/request.js
@@ -59,7 +59,7 @@ Page({
   openPhoto(){
     let t = this;
     wx.chooseImage({
-      count: 1, // 默认9
+      count: 999, // 默认9
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: function (res) {
--- weChat-mpvue/src/pages/second-index/index.vue
+++ weChat-mpvue/src/pages/second-index/index.vue
@@ -6,53 +6,8 @@
     </section>
 
     <section class="content">
-      <scroll-view class="scroll-view_H" scroll-y >
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
-        <view class="scroll-view-item"><image src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"></image></view>
+      <scroll-view class="scroll-view_H" scroll-y>
+        <view  v-for="(v,i) in imgaList" :key="i" class="scroll-view-item"><image :src="v" @click="previewImage(v)"></image></view>
       </scroll-view>
     </section>
 
@@ -72,7 +27,48 @@
   export default {
     data(){
       return {
-
+        imgaList:[
+          "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2129846603,1401619372&fm=200&gp=0.jpg",
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1522732982131&di=d6b073bb1a7e2882772c66e9b3003935&imgtype=jpg&src=http%3A%2F%2Fimg0.imgtn.bdimg.com%2Fit%2Fu%3D1017759020%2C224743705%26fm%3D214%26gp%3D0.jpg",
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1522732996912&di=c92e36ef014aae764d6e711678daa153&imgtype=0&src=http%3A%2F%2Fpic2.ooopic.com%2F10%2F87%2F42%2F18b1OOOPIC40.jpg",
+          "https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/w%3D268%3Bg%3D0/sign=f86459adeddde711e7d244f09fd4a926/dbb44aed2e738bd49e1a5b99a98b87d6277ff95b.jpg",
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1522733020840&di=2b394f175f7e915666cec462fec1554b&imgtype=0&src=http%3A%2F%2Fpic1.vip.com%2Fupload%2Fmerchandise%2F187761%2FMQD-M142W1306305-15_1.jpg",
+          "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2798323348,3182669990&fm=200&gp=0.jpg",
+          "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3418043705,3523926523&fm=27&gp=0.jpg",
+          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1522733066950&di=ac7eb264947f24fcfcf7be37b6e6ede7&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimage%2Fc0%253Dpixel_huitu%252C0%252C0%252C294%252C40%2Fsign%3D48858afd01d162d991e36a5c78a7cc80%2F0b46f21fbe096b63bca99a6907338744ebf8ac53.jpg",
+          "http://img.jj20.com/up/allimg/911/120515140005/151205140005-1-lp.jpg",
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg"
+        ]
+      }
+    },
+    methods:{
+      previewImage: function (current) {
+        let t = this;
+        wx.previewImage({
+          current: current, // 当前显示图片的http链接
+          urls: t.imgaList // 需要预览的图片http链接列表
+        })
       }
     }
   }
